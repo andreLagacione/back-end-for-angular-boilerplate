c7fca727b66d0e8cb30a3ac29bc0e2cbb43e0546
andreLagacione <lagacioneandre@gmail.com>
2020-01-12 20:54:08 -0300

    create new route

--- api/index.js
+++ api/index.js
@@ -5,5 +5,6 @@ router.use('/companies', require('./companies'));
 router.use('/menus', require('./menus'));
 router.use('/permissions', require('./permissions'));
 router.use('/users', require('./users'));
+router.use('/users/companies/combo-list', require('./user-companies'));
 
 module.exports = router;
